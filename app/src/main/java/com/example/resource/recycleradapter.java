b2e9c196b556fe9e2c4bdef8897dc7a5d50d1d06
ruchita <ruchitdamodar2000@gmail.com>
2020-06-20 18:37:14 +0530

    android file manager

---
+++ app/src/main/java/com/example/resource/recycleradapter.java
@@ -0,0 +1,52 @@
+package com.example.resource;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.BreakIterator;
+import java.util.List;
+
+public class recycleradapter extends RecyclerView.Adapter<recycleradapter.myViewHolder>{
+
+    private List<String> files;
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.listview,parent,false);
+        return new myViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        String title=files.get(position);
+        holder.uploadedfilename.setText(title);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return files.size();
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView down,delete;
+        TextView uploadedfilename;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            down=itemView.findViewById(R.id.downloadimg);
+            delete=itemView.findViewById(R.id.deleteimg);
+            uploadedfilename=itemView.findViewById(R.id.uploadedfilename);
+
+        }
+    }
+}
